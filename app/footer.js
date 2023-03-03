ffc7f520a73cefa55582adbba02e9400c9d8b9b9
idamakela <idamakela98@gmail.com>
2023-03-03 11:53:06 +0100

    made footer, icon not centered tho

--- app/footer.js
+++ app/footer.js
@@ -1,9 +1,20 @@
 import Link from 'next/link';
+import { FaRegSmileBeam } from 'react-icons/fa'
+
 
 export default function FooterLayout() {
     return (
+        <>
         <footer>
+            <div className='footer-items'>
+                <Link href='/'>Home</Link>
+                <Link href='/projects'>Projects</Link>
+                <Link href='/about'>About</Link>
+                <Link href='/contact'>Contact</Link>
+            </div>
+            <FaRegSmileBeam className='icon'/>
             <h2>&copy; Ida Mäkelä.</h2>
         </footer>
+        </>
     )
 }
