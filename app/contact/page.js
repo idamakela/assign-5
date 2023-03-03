57d9eea244a8d15227556dc443ace2a9c95cc379
idamakela <idamakela98@gmail.com>
2023-03-03 15:28:02 +0100

    inserted background image for form

--- app/contact/page.js
+++ app/contact/page.js
@@ -9,6 +9,11 @@ export default function Contact() {
                 <h3>Want to know more about my projects, or simply have a chat over a cup of coffee? Send me a message in the form below!</h3>
             </div>
         </div>
+
+        <div className='form-container'>
+            form container
+        </div>
+        <p className='img-ref'>Photo by <a href="https://unsplash.com/@emilybernal?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Emily Bernal</a> on <a href="https://unsplash.com/photos/r2F5ZIEUPtk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>.</p>
         </>
     )
 }
