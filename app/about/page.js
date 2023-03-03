b1324dcc6cbbc0bd022361136f10053b55961518
idamakela <idamakela98@gmail.com>
2023-03-03 14:20:01 +0100

    started on about me and fixed some variables

--- app/about/page.js
+++ app/about/page.js
@@ -1,3 +1,35 @@
+import '../../styles/about.scss'
+
 export default function About() {
-    return <h1>this is the about page</h1>
+    return (
+        <>
+        <div className="container">
+            <div className="text">
+                <h2>About me.</h2>
+                <h3>*...some short description about myself...*</h3>
+            </div>
+        </div>
+        <div className='grid-container'>
+            <div className='div1'>
+                <h3>title</h3>
+                <p>some text...</p>
+            </div>
+
+            <div className='div2'>
+                <h3>title</h3>
+                <p>some text...</p>
+            </div>
+
+            <div className='div3'>
+                <h3>title</h3>
+                <p>some text...</p>
+            </div>
+
+            <div className='div4'>
+                <h3>title</h3>
+                <p>some text...</p>
+            </div>
+        </div>    
+        </>
+    )
 }
