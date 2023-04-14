45ccd66d659e3afd60b716b4a8fa979c61cdaca4
Ida Mäkelä <idamakela98@gmail.com>
2023-04-14 11:26:22 +0200

    made contact form more compact

--- app/contact/form.js
+++ app/contact/form.js
@@ -5,31 +5,28 @@ export default function Form() {
             method="POST"
             enctype="text/plain"
         >
-            <label for="name">Name</label>
             <input
                 type="text"
                 id="name"
                 name="name"
-                placeholder="Josi Johnsson"
+                placeholder="Name"
                 required
             />
 
-            <label for="email">Email</label>
             <input
                 type="email"
                 id="email"
                 name="email"
-                placeholder="JosiJ@email-address.com"
+                placeholder="Email"
                 required
             />
 
-            <label for="message">Message</label>
             <textarea
                 id="message"
                 name="message"
                 rows="5"
                 colu="10"
-                placeholder="Write your message here!"
+                placeholder="Message"
                 required
             ></textarea>
 
