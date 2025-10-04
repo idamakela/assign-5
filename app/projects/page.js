ccd6f8f74aceddbdadbf0fd67a6d765c4d79579f
Ida Mäkelä <idamakela98@gmail.com>
2025-10-04 18:13:46 +0200

    refactor: changed order of data

--- app/projects/page.js
+++ app/projects/page.js
@@ -2,11 +2,9 @@ import data from '../../public/data.js';
 import Project from './project.js';
 
 export default function SpecProject() {
-    const reverseData = data.reverse();
-
     return (
         <div className="projects">
-            {reverseData.map((project, index) => (
+            {data.map((project, index) => (
                 <Project key={index} {...project}/>
             ))}
         </div>
