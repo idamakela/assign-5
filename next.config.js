c1070baeb3c6b737ec87d40d1c0b8d0d6066e185
Ida Mäkelä <idamakela98@gmail.com>
2025-10-04 18:24:14 +0200

    chore: npm package updates

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const path = require('path');
 
 const nextConfig = {
-    experimental: {
-        appDir: true,
-    },
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
